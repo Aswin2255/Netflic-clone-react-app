64c8b36137e9224730e8404b211511420418387b
aswinkumar <mynameisaswin321@gmail.com>
2022-11-30 00:19:40 -0500

    ui completed

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <div className='banner-bt'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='butn'>Play</div>
+            <div className='butn'>My list</div>
+            </div>
+        
+        <h1 className='description' >In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</h1>
+      
+    </div>
+    <div className='fade-bot'></div>
+    </div>
+  )
+}
+
+export default Banner
